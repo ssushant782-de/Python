ad2400371f77b997230432832afc66ab639cf5a4
sushantdevops <sushantsawant05@gmail.com>
2026-01-10 17:42:58 -0500

    updated the random password logic

--- task.py
+++ task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -6,5 +8,21 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+password = ""
+for i in range(nr_letters):
+    letter=random.choice(letters)
+    password += letter
+for j in range(nr_symbols):
+    symbol=random.choice(symbols)
+    password += symbol
+for k in range(nr_numbers):
+    number = random.choice(numbers)
+    password += number
+
+char_password = list(password) #converts the string to list of characters
+random.shuffle(char_password) #shuffle the list in-place
+shuffled_password = "".join(char_password) #join the list back to the string
+print(shuffled_password)
+
 
 
